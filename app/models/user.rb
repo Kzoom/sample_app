1fb2813a01f1162ff14f8c0f1f9061d2fc67048a
Kelley Willis <kelleywillis99@gmail.com>
2014-09-24 20:40:40 -0700

    did Chap 6 exercises for better REG_EX

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ActiveRecord::Base
 	# standardize on lower-case email addresses, 
 	#  in case database in case-sensitive
-	before_save { self.email = email.downcase}
+  
+  # 2014-09-24 ksw...self isn't needed on the right side
+  #    and the '!' bang makes this even shorter
+	#before_save { self.email = email.downcase}
+	
+	before_save { email.downcase!}
 
 	validates :name, presence: true, length: {maximum: 50}
 	
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}, 
+	# 2014-09-24 ksw...the first regex allows consecutive dots, '..'
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]{2,4}+\z/i
+	validates :email, presence: true, 
+	                  format: {with: VALID_EMAIL_REGEX}, 
 	                  uniqueness: {case_sensitive: false}
 
 	has_secure_password
